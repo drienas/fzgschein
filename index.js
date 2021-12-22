d202e6571c91d1069ff1b5847e98b7420836e18a
Daniel Rienas <daniel.rienas@dieschneidergruppe.de>
2021-12-22 15:28:07 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,43 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+let iConnection, iChannel;
+
+const rmq = require('./rmq');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/data', async (req, res) => {
+  let body = req.body;
+  if (!body.values) {
+    res.status(400).send(`Kein gültiges Format`);
+  } else {
+    let values =
+      typeof body.values === 'string' ? JSON.parse(body.values) : body.values;
+    let picture = values['_0'] || null;
+    let customerNumber = values['_1'] || null;
+    let cnum = values['_2'] || null;
+
+    let data = JSON.stringify({ picture, customerNumber, cnum });
+
+    await iChannel.publish(
+      'ai_data',
+      'insert.fahrzeugschein',
+      Buffer.from(JSON.stringify(data))
+    );
+
+    res.json(data);
+  }
+});
+
+app.listen(3000, async () => {
+  iConnection = await rmq.connect();
+  iChannel = await rmq.initExchangeChannel(iConnection, 'ai_data');
+  console.log(`App running on port 3000`);
+});
